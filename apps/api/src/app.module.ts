93b9fc2ee69b1418041aac5305d2c6ab2e2fa757
Soumajit Ghosh <soumojitghosh02@gmail.com>
2025-01-25 23:24:58 +0530

    git ignore updated and config files added

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,34 @@
 import {Module} from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import {AuthModule} from './modules/auth/auth.module';
+import {UserModule} from './modules/user/user.module';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {configValidationSchema} from "./config.schema";
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.STAGE === 'prod',
+      envFilePath: [`.env.stage.${process.env.STAGE}`, '.env'],
+      validationSchema: configValidationSchema,
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: configService.get('STAGE') !== 'prod',
+        };
+      },
+    }), AuthModule, UserModule],
 })
 export class AppModule {
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interseptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
