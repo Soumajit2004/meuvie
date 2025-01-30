782afb94ed9e76be47656fdbe6c760d6e8bd3f85
Soumajit Ghosh <soumojitghosh02@gmail.com>
2025-01-30 21:45:12 +0530

    duplicate session creation handling implemented

--- apps/api/src/modules/auth/user-session.repository.ts
+++ apps/api/src/modules/auth/user-session.repository.ts
@@ -5,32 +5,37 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 
 @Injectable()
-export class UserSessionRepository extends Repository<UserSession> {
+export class UserSessionRepository {
   constructor(
     @InjectRepository(UserSession) private userSessionRepository: Repository<UserSession>,
   ) {
-    super(userSessionRepository.target, userSessionRepository.manager, userSessionRepository.queryRunner);
   }
 
   async createSession(sessionId: string, user: User, expiresAt: Date): Promise<UserSession> {
+    const previousSession = await this.userSessionRepository.findOneBy({ user: user });
+
+    if (previousSession) {
+      await this.deleteSession(previousSession.id);
+    }
+
     const session = new UserSession();
 
     session.id = sessionId;
     session.user = user;
     session.expiresAt = expiresAt;
 
-    return this.save(session);
+    return this.userSessionRepository.save(session);
   }
 
   async deleteSession(sessionId: string): Promise<void> {
-    await this.delete(sessionId);
+    await this.userSessionRepository.delete(sessionId);
   }
 
   async findUserSessionById(sessionId: string): Promise<UserSession | null> {
-    return this.findOne({ where: { id: sessionId }, relations: ['user'] });
+    return this.userSessionRepository.findOne({ where: { id: sessionId }, relations: ['user'] });
   }
 
   async updateSession(session: UserSession): Promise<UserSession> {
-    return this.save(session);
+    return this.userSessionRepository.save(session);
   }
 }
