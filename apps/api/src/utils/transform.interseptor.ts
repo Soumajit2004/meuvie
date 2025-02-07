1bfc746b1fc01e2ba4763ec536050a73ab5f6871
Soumajit Ghosh <soumojitghosh02@gmail.com>
2025-02-07 15:01:35 +0530

    feat: common response object implemented

--- apps/api/src/utils/transform.interseptor.ts
+++ apps/api/src/utils/transform.interseptor.ts
@@ -6,20 +6,30 @@ import {
 } from '@nestjs/common';
 import { instanceToPlain } from 'class-transformer';
 import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+
+export interface Response<T> {
+  statusCode: number;
+  data: T;
+}
 
 @Injectable()
-export class TransformInterceptor implements NestInterceptor {
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
   /**
    * Intercepts the request and transforms the response data to plain objects.
    * @param {ExecutionContext} context - The execution context.
    * @param {CallHandler<any>} next - The call handler.
    * @returns {Observable<any>} An observable that emits the transformed data.
    */
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<any> {
-    return next.handle().pipe(map((data) => instanceToPlain(data)));
+  intercept(context: ExecutionContext, next: CallHandler<any>): any {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          statusCode: context.switchToHttp().getResponse().statusCode,
+          data: instanceToPlain(data),
+        };
+      }),
+    );
   }
 }
